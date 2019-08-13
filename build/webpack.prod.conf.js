010f1a2c04d4ef6f139c6d303f34170f22f4de12
Erickson <bingluen0224@gmail.com>
2019-08-14 02:07:10 +0800

    Remove CNAME
    Release domain

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -116,10 +116,6 @@ const webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
-      },
-      {
-        from: path.resolve(__dirname, '../CNAME'),
-        to: config.build.assetsRoot
       }
     ])
   ]
